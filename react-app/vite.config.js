dff40b3abefeb328d02d0831ced191f4aac8db17
Romany Ghoubrial <romany.ghoubrial@gmail.com>
2026-04-16 01:34:44 +0200

    fix react build inside wp plugin

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "build",
+    assetsDir: "",
+    rollupOptions: {
+      output: {
+        entryFileNames: "index.js",
+        assetFileNames: "index.css"
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3000"
